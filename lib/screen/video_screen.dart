fcf8a58d528ade1902827d24beebb5dff4187e70
1x3x3x7 <galacticdeveloper@protonmail.com>
2021-06-05 01:59:25 +0300

    add video player

--- lib/screen/video_screen.dart
+++ lib/screen/video_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoScreen extends StatelessWidget {
   @override
@@ -7,7 +8,53 @@ class VideoScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Video"),
       ),
-      body: Text("BODY"),
+      body: VideoWidget(),
+    );
+  }
+}
+
+class VideoWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _VideoWidgetState();
+  }
+}
+
+class _VideoWidgetState extends State<VideoWidget> {
+  late VideoPlayerController videoPlayerController;
+
+  initializeVideoPlayer() {
+    videoPlayerController = VideoPlayerController.network(
+        'https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4');
+    videoPlayerController.initialize().then((_) {
+      setState(() {});
+      videoPlayerController.play();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializeVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: videoPlayerController.value.isInitialized
+          ? AspectRatio(
+              aspectRatio: videoPlayerController.value.aspectRatio,
+              child: VideoPlayer(videoPlayerController),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
