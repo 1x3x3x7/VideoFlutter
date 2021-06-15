d9311f0d04832e9cfcbce0a7bb140f95f40c0b7c
1x3x3x7 <galacticdeveloper@protonmail.com>
2021-06-15 21:26:37 +0300

    add card list

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,17 +13,35 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Video App"),
-      ),
-      body: Container(
-        color: Colors.white,
-        child: Center(
-            child: TextButton(
-                child: Text("open next screen"),
-                onPressed: () => StoreProvider.of<AppState>(context)
-                    .dispatch(NavigatePushAction(AppRoutes.video_screen)))),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Video App"),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, int index) {
+              return Card(
+                  margin: EdgeInsets.all(10),
+                  child: Container(
+                    width: double.infinity, //stretches to maximum width
+                    height: 200.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "https://images.unsplash.com/photo-1611861615311-4409a7dc648d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2178&q=80"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: TextButton(
+                        child: Text("Video $index"),
+                        onPressed: () => StoreProvider.of<AppState>(context)
+                            .dispatch(
+                                NavigatePushAction(AppRoutes.video_screen))),
+                  ));
+            },
+          ),
+        ));
   }
 }
--- lib/screen/video_screen.dart
+++ lib/screen/video_screen.dart
@@ -53,11 +53,11 @@ class _VideoWidgetState extends State<VideoWidget> {
   List<BetterPlayerDataSource> createDataSet() => [
         BetterPlayerDataSource(
           BetterPlayerDataSourceType.network,
-          'https://vod-progressive.akamaized.net/exp=1623025993~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4204%2F16%2F421020269%2F1819002288.mp4~hmac=7d3a1103824d863f13b6701fd2af6ca07f3ef93e9197ee3c5a214299f5d87ab6/vimeo-prod-skyfire-std-us/01/4204/16/421020269/1819002288.mp4?filename=Cycling+-+39183.mp4',
+          'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4',
         ),
         BetterPlayerDataSource(BetterPlayerDataSourceType.network,
-            'https://vod-progressive.akamaized.net/exp=1623026800~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4973%2F14%2F374868332%2F1561932694.mp4~hmac=2b5f235f0a26c72339661cc6ba7ee7a9873005ea3000b8b157ac93fd6dab5780/vimeo-prod-skyfire-std-us/01/4973/14/374868332/1561932694.mp4?filename=Bodybuilding+-+29328.mp4'),
+            'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'),
         BetterPlayerDataSource(BetterPlayerDataSourceType.network,
-            'https://vod-progressive.akamaized.net/exp=1623026827~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F2436%2F18%2F462182255%2F2046344646.mp4~hmac=6cbc205374d7870415297cf7cf066a8470ddeec1a1bde5f6603cd4ab795c6577/vimeo-prod-skyfire-std-us/01/2436/18/462182255/2046344646.mp4?filename=Workout+-+50888.mp4'),
+            'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4'),
       ];
 }
